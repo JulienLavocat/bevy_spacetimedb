3226c3df515b70cc41d9aacce67c0c3dc996c553
Jeffrey Rooks <onx2rj@gmail.com>
2025-10-03 09:18:56 +0100

    simplify export

--- bevy_spacetimedb/src/lib.rs
+++ bevy_spacetimedb/src/lib.rs
@@ -14,12 +14,8 @@ pub use aliases::*;
 #[cfg(feature = "macros")]
 pub use bevy_spacetimedb_macros::*;
 
-// Primary message-based API
 pub use channel_receiver::AddMessageChannelAppExtensions;
-pub use messages::{
-    DeleteMessage, InsertMessage, InsertUpdateMessage, ReducerResultMessage, StdbConnectedMessage,
-    StdbConnectionErrorMessage, StdbDisconnectedMessage, UpdateMessage,
-};
+pub use messages::*;
 pub use plugin::*;
 pub use reducers::RegisterableReducerMessage;
 pub use stdb_connection::*;
